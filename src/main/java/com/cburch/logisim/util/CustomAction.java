91706d6d95487586bc48ebca801ec12078a583f4
Simon Hunt <simon@opennetworking.org>
2017-11-30 11:30:36 -0800

    CustomAction: clean up class and add unit tests.

--- src/main/java/com/cburch/logisim/util/CustomAction.java
+++ src/main/java/com/cburch/logisim/util/CustomAction.java
@@ -1,33 +1,88 @@
+/*
+ * Copyright (c) 2010, Carl Burch. License information is located in the
+ * com.cburch.logisim.Main source code and at www.cburch.com/logisim/.
+ */
+
 package com.cburch.logisim.util;
-
-import java.awt.KeyboardFocusManager;
-import java.awt.event.ActionEvent;
-
-import javax.swing.AbstractAction;
 
 import com.cburch.logisim.gui.generic.ZoomControl;
 import com.cburch.logisim.gui.menu.MenuSimulate;
 
-@SuppressWarnings("serial")
-public class CustomAction extends AbstractAction {
-	private String cmd;
-	private Object obj;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
+import static java.awt.KeyboardFocusManager.getCurrentKeyboardFocusManager;
+
+/**
+ * A custom action.
+ * Note that this implementation explicitly handles keyboard events for
+ * the main canvas:
+ * <ul>
+ * <li>zoom-in</li>
+ * <li>zoom-out</li>
+ * <li>simulation-tick</li>
+ * </ul>
+ */
+// TODO: Consider renaming this class to CanvasKeyboardAction
+//       ... and placing it in the package containing Canvas
+public class CustomAction extends AbstractAction {
+    private static final Class<?> CANVAS_CLASS =
+            com.cburch.logisim.gui.main.Canvas.class;
+
+    private static final String E_NULL_DISALLOWED =
+            "command / object must not be null";
+
+    /**
+     * Designates key for zoom-in action (ctrl/cmd-plus).
+     */
+    public static final String KEY_ZOOM_IN = "CTRL+";
+
+    /**
+     * Designates key for zoom-out action (ctrl/cmd-minus).
+     */
+    public static final String KEY_ZOOM_OUT = "CTRL-";
+
+    /**
+     * Designates key for simulation-tick action (space bar).
+     */
+    public static final String KEY_SIM_TICK = "Space";
+
+
+    private final String cmd;
+    private final Object obj;
+
+    /**
+     * Creates a custom action.
+     *
+     * @param cmd the command associated with this action
+     * @param obj the object upon which the action is performed
+     * @throws NullPointerException if either parameter is null
+     */
     public CustomAction(String cmd, Object obj) {
+        if (cmd == null || obj == null) {
+            throw new NullPointerException(E_NULL_DISALLOWED);
+        }
         this.cmd = cmd;
         this.obj = obj;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-    	if (KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusOwner().getClass() == com.cburch.logisim.gui.main.Canvas.class) {
-	        if (cmd.equalsIgnoreCase("CTRL+")) {
-	            ((ZoomControl) obj).zoomIn();
-	        } else if (cmd.equalsIgnoreCase("CTRL-")) {
-	        	((ZoomControl) obj).zoomOut();
-	        } else if (cmd.equalsIgnoreCase("Space")) {
-	        	((MenuSimulate) obj).tick();
-	        }
-    	}
+        if (keyboardFocusedOnCanvas()) {
+            if (cmd.equalsIgnoreCase(KEY_ZOOM_IN)) {
+                ((ZoomControl) obj).zoomIn();
+            } else if (cmd.equalsIgnoreCase(KEY_ZOOM_OUT)) {
+                ((ZoomControl) obj).zoomOut();
+            } else if (cmd.equalsIgnoreCase(KEY_SIM_TICK)) {
+                ((MenuSimulate) obj).tick();
+            }
+        }
+    }
+
+    boolean keyboardFocusedOnCanvas() {
+        // NOTE: protect against focusOwner being null
+        Component focusOwner = getCurrentKeyboardFocusManager().getFocusOwner();
+        return focusOwner != null && focusOwner.getClass().equals(CANVAS_CLASS);
     }
 }
---
+++ src/test/java/com/cburch/logisim/util/CustomActionTest.java
@@ -0,0 +1,163 @@
+/*
+ *  Copyright (c) 2017, Simon Hunt et al.
+ *  License information is located in the com.cburch.logisim.Main source code.
+ *
+ */
+
+package com.cburch.logisim.util;
+
+import com.cburch.logisim.gui.generic.ZoomControl;
+import com.cburch.logisim.gui.generic.ZoomModel;
+import com.cburch.logisim.gui.menu.LogisimMenuBar;
+import com.cburch.logisim.gui.menu.MenuSimulate;
+import com.meowster.test.AbstractTest;
+import org.junit.Test;
+
+import java.beans.PropertyChangeListener;
+
+import static com.cburch.logisim.util.CustomAction.KEY_SIM_TICK;
+import static com.cburch.logisim.util.CustomAction.KEY_ZOOM_IN;
+import static com.cburch.logisim.util.CustomAction.KEY_ZOOM_OUT;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Unit tests for {@link CustomAction}.
+ */
+public class CustomActionTest extends AbstractTest {
+
+    private class ZoomModelAdapter implements ZoomModel {
+
+        @Override
+        public void addPropertyChangeListener(String prop, PropertyChangeListener l) {
+        }
+
+        @Override
+        public void removePropertyChangeListener(String prop, PropertyChangeListener l) {
+        }
+
+        @Override
+        public boolean getShowGrid() {
+            return false;
+        }
+
+        @Override
+        public double getZoomFactor() {
+            return 0;
+        }
+
+        @Override
+        public double[] getZoomOptions() {
+            return new double[0];
+        }
+
+        @Override
+        public void setShowGrid(boolean value) {
+        }
+
+        @Override
+        public void setZoomFactor(double value) {
+        }
+    }
+
+    private class MockZoomControl extends ZoomControl {
+        private boolean weZoomedIn = false;
+        private boolean weZoomedOut = false;
+
+        private MockZoomControl() {
+            super(new ZoomModelAdapter());
+        }
+
+        @Override
+        public void zoomIn() {
+            weZoomedIn = true;
+        }
+
+        @Override
+        public void zoomOut() {
+            weZoomedOut = true;
+        }
+    }
+
+    private class MockMenuBar extends LogisimMenuBar {
+        MockMenuBar() {
+            super(null, null);
+        }
+    }
+
+    private class MockMenuSimulate extends MenuSimulate {
+        private boolean weTicked = false;
+
+        private MockMenuSimulate() {
+            super(new MockMenuBar());
+        }
+
+        @Override
+        public void tick() {
+            weTicked = true;
+        }
+    }
+
+
+    private class TestableCustomAction extends CustomAction {
+        TestableCustomAction(String cmd, Object obj) {
+            super(cmd, obj);
+        }
+
+        @Override
+        boolean keyboardFocusedOnCanvas() {
+            // fake the system into thinking a canvas has keyboard focus
+            return true;
+        }
+    }
+
+    private CustomAction action;
+
+    /*
+     * TODO: Investigate why these unit tests take so long to initialize...
+     *       Presumably it is the setup of the ZoomControl / MenuSimulate
+     */
+
+    @Test
+    public void zoomInAction() {
+        title("zoom-in action");
+        MockZoomControl mockZoomer = new MockZoomControl();
+        action = new TestableCustomAction(KEY_ZOOM_IN, mockZoomer);
+
+        assertThat(mockZoomer.weZoomedIn, is(false));
+        action.actionPerformed(null);
+        assertThat(mockZoomer.weZoomedIn, is(true));
+    }
+
+    @Test
+    public void zoomOutAction() {
+        title("zoom-out action");
+        MockZoomControl mockZoomer = new MockZoomControl();
+        action = new TestableCustomAction(KEY_ZOOM_OUT, mockZoomer);
+
+        assertThat(mockZoomer.weZoomedOut, is(false));
+        action.actionPerformed(null);
+        assertThat(mockZoomer.weZoomedOut, is(true));
+    }
+
+    @Test
+    public void simTickAction() {
+        title("sim-tick action");
+        MockMenuSimulate mockMenuSim = new MockMenuSimulate();
+        action = new TestableCustomAction(KEY_SIM_TICK, mockMenuSim);
+
+        assertThat(mockMenuSim.weTicked, is(false));
+        action.actionPerformed(null);
+        assertThat(mockMenuSim.weTicked, is(true));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullCommand() {
+        new CustomAction(null, new Object());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullObject() {
+        new CustomAction(KEY_SIM_TICK, null);
+    }
+}
