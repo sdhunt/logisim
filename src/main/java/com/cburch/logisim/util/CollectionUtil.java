a9884b4c5894a98eff23e695946436f91d83ffb3
Simon Hunt <simon@opennetworking.org>
2017-11-29 20:59:54 -0800

    CollectionUtil: Fixed set union issues.
    - actually deleted the inner class, and rewrote the static method.
      (unit tests pass now)

--- src/main/java/com/cburch/logisim/util/CollectionUtil.java
+++ src/main/java/com/cburch/logisim/util/CollectionUtil.java
@@ -5,37 +5,14 @@
 
 package com.cburch.logisim.util;
 
-import org.apache.commons.collections15.iterators.IteratorChain;
-
-import java.util.AbstractSet;
-import java.util.Iterator;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Custom utilities for collections.
  */
 public class CollectionUtil {
-
-    private static class UnionSet<E> extends AbstractSet<E> {
-        private Set<? extends E> a;
-        private Set<? extends E> b;
-
-        UnionSet(Set<? extends E> a, Set<? extends E> b) {
-            // FIXME: does not handle same items in both sets!!
-            this.a = a;
-            this.b = b;
-        }
-
-        @Override
-        public int size() {
-            return a.size() + b.size();
-        }
-
-        @Override
-        public Iterator<E> iterator() {
-            return new IteratorChain<>(a.iterator(), b.iterator());
-        }
-    }
 
     // no instantiation from outside this class
     private CollectionUtil() {
@@ -49,14 +26,14 @@ public class CollectionUtil {
      * @param <E> the set element type
      * @return a set union of the two sets
      */
-    // TODO: review - despite the method name, the returned set is mutable!
-    // To ensure immutability, the UnionSet constructor should make copies
-    // of sets a and b.
     public static <E> Set<E> createUnmodifiableSetUnion(Set<? extends E> a,
                                                         Set<? extends E> b) {
         if (a == null || b == null) {
             throw new NullPointerException("null is not permitted");
         }
-        return new UnionSet<>(a, b);
+        Set<E> combo = new HashSet<>();
+        combo.addAll(a);
+        combo.addAll(b);
+        return Collections.unmodifiableSet(combo);
     }
 }
