73b65b17ad042aa6453348d6f5c6fb4f2aea5442
Simon Hunt <simon@opennetworking.org>
2017-12-21 13:24:18 -0800

    Javadocs for interfaces.

--- src/main/java/com/cburch/logisim/util/JInputComponent.java
+++ src/main/java/com/cburch/logisim/util/JInputComponent.java
@@ -1,9 +1,25 @@
-/* Copyright (c) 2010, Carl Burch. License information is located in the
- * com.cburch.logisim.Main source code and at www.cburch.com/logisim/. */
+/*
+ * Copyright (c) 2010, Carl Burch. License information is located in the
+ * com.cburch.logisim.Main source code and at www.cburch.com/logisim/.
+ */
 
 package com.cburch.logisim.util;
 
+/**
+ * Represents an input component.
+ */
 public interface JInputComponent {
-    public Object getValue();
-    public void setValue(Object value);
+    /**
+     * Returns the value from the component.
+     *
+     * @return the value
+     */
+    Object getValue();
+
+    /**
+     * Sets the value on the component.
+     *
+     * @param value the value to set
+     */
+    void setValue(Object value);
 }
--- src/main/java/com/cburch/logisim/util/WindowClosable.java
+++ src/main/java/com/cburch/logisim/util/WindowClosable.java
@@ -1,8 +1,17 @@
-/* Copyright (c) 2011, Carl Burch. License information is located in the
- * com.cburch.logisim.Main source code and at www.cburch.com/logisim/. */
+/*
+ * Copyright (c) 2011, Carl Burch. License information is located in the
+ * com.cburch.logisim.Main source code and at www.cburch.com/logisim/.
+ */
 
 package com.cburch.logisim.util;
 
+/**
+ * An entity that represents a window that can be closed.
+ */
 public interface WindowClosable {
-    public void requestClose();
+
+    /**
+     * Invoked by an external event to request the window closes.
+     */
+    void requestClose();
 }
